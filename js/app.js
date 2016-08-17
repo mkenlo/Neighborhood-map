32edc639c230e74172ec7c693ab183ff90a79c68
mkenlo <mel.lontsi@gmail.com>
2016-08-17 17:51:42 -0400

    fix filter feature;

--- js/app.js
+++ js/app.js
@@ -8,9 +8,10 @@ var FS_CLIENT_SECRET = "WBU23PY5WFX1F3TEM0MJ1DTSVIDPBWN1QNA1PKNP14HKM3AP";
 var FS_URL= "https://api.foursquare.com/v2/venues/search?client_id=%clt_id%&client_secret=%clt_scrt%&v=20130815&near=%city%";
 
 
-var markers=[];
+var venues=[];
 
-// defaults markers to initialise the map
+
+// defaults markers to initialize the map
 defaultMarkers =[
         new Locations("Ike's Food & Cocktails","(612) 746-4537","50 S 6th St","",44.97818705436708,-93.27229499816895),
         new Locations("Eli's East Food & Cocktails","(612) 331-0031","815 E Hennepin Ave","#",44.99128282822349,-93.24738264083862),
@@ -26,72 +27,91 @@ function initMap() {
       center: {lat: 38.9165087, lng: -77.2482606}, // coordinates for a random place in Minneapolis, MN
       zoom: 13
     }
-    map = new google.maps.Map(document.getElementById('map'), mapOptions);   
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    //set markers
+    for(i=0; i< defaultMarkers.length; i++){
+        var position = new google.maps.LatLng(defaultMarkers[i].lat, defaultMarkers[i].lng);
+        if(!defaultMarkers[i].marker()){
+          
+            defaultMarkers[i].marker(new google.maps.Marker({
+            position: position,
+            map: map,
+            title: defaultMarkers[i].name
+            }));
+        }
+    }    
     // add defaults markers on map
     updateMap(map, defaultMarkers);
 
 }
 
-function toggleBounce() {
-
-  if (marker.getAnimation() !== null) {
-    marker.setAnimation(null);
-  } else {
-    marker.setAnimation(google.maps.Animation.BOUNCE);
-  }
+function removeDefaultMarkers(){
+    
+     for(i=0; i< defaultMarkers.length; i++){
+        
+        if(defaultMarkers[i].marker()){
+            console.log("marker "+i);
+            defaultMarkers[i].marker().setMap(null);
+            
+        }
+    }
+    
 }
 
 function updateMap(map, markers){
     
     var bounds = new google.maps.LatLngBounds();
+    /* add Listener for each marker*/
+    var infoWindow = new google.maps.InfoWindow();
+    
     // add marker on map
     for( i = 0; i < markers.length; i++ ) {
-        var content = "<div class='panel panel-marker'><div class='panel-heading'>%name%</div><div class='panel-body'>%description%</div></div>";
         var position = new google.maps.LatLng(markers[i].lat, markers[i].lng);
-        bounds.extend(position);
-        marker = new google.maps.Marker({
-            position: position,
-            map: map,
-            animation: google.maps.Animation.DROP,
-            title: markers[i].name
-        });
-        // add content information for each marker
-        content = content.replace("%name%",markers[i].name);
-        description = "<ul class='panel-marker-list'>";
-        description += "<li><span class='glyphicon glyphicon-earphone'></span> "+markers[i].contact+"</li>";
-        description += "<li><span class='glyphicon glyphicon-map-marker'></span> "+markers[i].fulladress+"</li>";
-        description += "<li><span class='glyphicon glyphicon-link'></span> &nbsp;<a href='"+markers[i].url+"'>"+markers[i].url+"</a></li>";
-        description += "</ul>";
-        content = content.replace("%description%",description);
         
-        /* add Listener for each marker*/
-        var infoWindow = new google.maps.InfoWindow()
-        
-        google.maps.event.addListener(marker, 'click', (function(marker,content) {
-            return function() {
-                infoWindow.setContent(content);
-                infoWindow.open(map, marker);
-            }
-        })(marker,content)); 
+        if (markers[i].showMe()){
 
-        // Event that closes the Info Window with a click on the map
-        google.maps.event.addListener(map, 'click', function() {
-            infoWindow.close();
-        });
-        // add some animations on marker
-        marker.addListener('click', toggleBounce);
-    }
+            bounds.extend(position);
+            var marker = markers[i].marker();          
+            var content = "<div class='panel panel-marker'><div class='panel-heading'>%name%</div><div class='panel-body'>%description%</div></div>";
+            content = content.replace("%name%",markers[i].name);
+            
+            var description = "<ul class='panel-marker-list'>";
+            description += "<li><span class='glyphicon glyphicon-earphone'></span> "+markers[i].contact+"</li>";
+            description += "<li><span class='glyphicon glyphicon-map-marker'></span> "+markers[i].fulladdress+"</li>";
+            description += "<li><span class='glyphicon glyphicon-link'></span> &nbsp;<a href='"+markers[i].url+"'>"+markers[i].url+"</a></li>";
+            description += "</ul>";
+
+            content = content.replace("%description%",description);
+
+           google.maps.event.addListener(marker, 'click', (function(marker,content) {
+                return function() {
+                    infoWindow.setContent(content);
+                    infoWindow.open(map, marker);
+                    marker.setAnimation(google.maps.Animation.BOUNCE);
+                    setTimeout(function(){ marker.setAnimation(null); }, 750);
+                }
+            })(marker,content)); 
+
+            // Event that closes the Info Window with a click on the map
+            google.maps.event.addListener(map, 'click', function() {
+                infoWindow.close();
+                
+            });
+        
+        } // end if       
+
+    } //end for
        
     map.fitBounds(bounds);
 
     // apply when window is resize
-    google.maps.event.addDomListener(window, 'load', initMap);
+    //google.maps.event.addDomListener(window, 'load', initMap);
     google.maps.event.addDomListener(window, "resize", function() {
-    var center = map.getCenter();
-    map.setZoom(8);
-    google.maps.event.trigger(map, "resize");
-    map.setCenter(center); 
-});
+        var center = map.getCenter();
+        map.setZoom(8);
+        google.maps.event.trigger(map, "resize");
+        map.setCenter(center); 
+    });
         
 }
 
@@ -103,12 +123,16 @@ function Locations(name, contact, fulladress, url, lat, lng){
     self.fulladress = fulladress;
     self.lat = lat;
     self.lng = lng;
-    
+    self.showMe = ko.observable(true);
+    self.marker = ko.observable();
+        
+  
 }
 
 
 function MapViewModel() {
     
+
     var self = this;
     self.city = ko.observable("Minneapolis,MN");
     self.fsError = ko.observable("");
@@ -116,61 +140,83 @@ function MapViewModel() {
     self.listVenues = ko.observableArray(defaultMarkers);
     
     self.filterList = function(data, event) {
-      
+        
         var filter = self.filter().toString().toLowerCase();
-        if(!filter) { 
-            updateMap(map, self.listVenues());
-            return self.listVenues;
-        } else {
-            result = [];
-           
+        if(filter) { 
+              
             $.each(self.listVenues(), function(i,item){
-                if(item.name.toLowerCase().startsWith(filter))                    
-                    result.push(item);
-                                    
+                test = item.name.toLowerCase().startsWith(filter);
+                if(!test) {
+                    item.marker().setVisible(false);
+                    item.showMe(false);
+                    
+                }
+                else if((event.keyCode == 8 || event.keyCode ==46) & !item.showMe()){
+                    item.marker().setVisible(true);
+                    item.showMe(true);
+                }                    
             });
             
-            if(result.length >0){
-                self.listVenues.removeAll();
-                updateMap(map, result);
-                return self.listVenues(result);
-            }
-            else{
-                updateMap(map, self.listVenues());
-                return self.listVenues;
-            }
-                      
         }
+        else{
+            //reset marker listVenue
+             $.each(self.listVenues(), function(i,item){
+                item.marker().setVisible(true);
+                item.showMe(true);
+                                  
+            });
+        }
+       
     };
   
-
+    self.clickVenue = function(venue){
+       
+        $.each(self.listVenues(), function(i,item){
+            if(item!=venue){
+                item.marker().setVisible(false);
+                item.showMe(false);
+                self.filter(venue.name);
+            }
+            
+                              
+        });
+        
+    }
     self.onSubmit = function(){
         
         var url = FS_URL.replace("%clt_id%",FS_CLIENT_ID);
         url = url.replace("%clt_scrt%", FS_CLIENT_SECRET);
         url = url.replace("%city%",self.city());
-       // url = url.concat("&query=",self.filter());
         url = url.concat("&limit=10");  
         
      
         $.getJSON(url,function(result){
+
+            removeDefaultMarkers();
             self.listVenues.removeAll();
+            
 
             $.each(result.response.venues, function(id, venue){
                 contact= venue.contact.formattedPhone;
                 address = venue.location.formattedAddress[0];
-                self.listVenues.push(new Locations(venue.name,contact, address, venue.url,venue.location.lat,venue.location.lng));
-                markers.push(new Locations(venue.name,contact, address,venue.url,venue.location.lat,venue.location.lng));                               
+
+                var location = new Locations(venue.name,contact, address, venue.url,venue.location.lat,venue.location.lng);
+                location.marker(new google.maps.Marker({
+                    position: new google.maps.LatLng(venue.location.lat,venue.location.lng),
+                    map: map,
+                    title: venue.name
+                    }));
+                self.listVenues.push(location);
+                
             });
            
-           updateMap(map, markers);
+            updateMap(map, self.listVenues());
            
-        }).error(function(){
+        }).fail(function(){
             self.fsError("Oops!! Sorry, for some reasons the request failed ");
-            console.log("FourSquare request failed");
+            
         });
     }
-  
    
 };
 
